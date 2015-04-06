4c1925490e028db834d5f717bd9520b19046e88e
David Watkins <dwatkins@insuremonkey.com>
2015-04-06 16:14:50 -0700

    Update to version 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-skin',
-    version='0.1dev',
+    version='0.1',
     packages=find_packages(),
     url='https://github.com/dwatkinsweb/django-skin',
     license='MIT',
