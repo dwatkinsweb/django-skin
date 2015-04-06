bd0a976fd9215bfbb2878f3511655befb5fb0467
David Watkins <dwatkins@insuremonkey.com>
2015-04-06 15:03:06 -0700

    use ugettext_lazy instead of gettext_lazy

--- skin/models.py
+++ skin/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.contrib.sites.models import Site
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class SiteSkin(models.Model):
