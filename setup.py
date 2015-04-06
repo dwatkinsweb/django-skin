487bec053c59c88256e3ddb686cab238a0332066
David Watkins <dwatkins@insuremonkey.com>
2015-04-06 07:50:57 -0700

    update setup file

--- setup.py
+++ setup.py
@@ -5,30 +5,9 @@ from __future__ import with_statement
 from setuptools import setup, find_packages
 
 
-# Figure out the version; this could be done by importing the
-# module, though that requires dependencies to be already installed,
-# which may not be the case when processing a pip requirements
-# file, for example.
-def parse_version(assignee):
-    import os
-    import re
-
-    here = os.path.dirname(os.path.abspath(__file__))
-    version_re = re.compile(
-        r'%s = (\(.*?\))' % assignee)
-    with open(os.path.join(here, 'skin', '__init__.py')) as fp:
-        for line in fp:
-            match = version_re.search(line)
-            if match:
-                version = eval(match.group(1))
-                return ".".join(map(str, version))
-        else:
-            raise Exception("cannot find version")
-
-
 setup(
     name='django-skin',
-    version=parse_version('__version__'),
+    version='0.1dev',
     packages=find_packages(),
     url='https://github.com/dwatkinsweb/django-skin',
     license='MIT',
@@ -43,8 +22,9 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
