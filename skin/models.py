85d1732dd05a65fe4d4d749a3d7ceed64461ab48
David Watkins <dwatkins@insuremonkey.com>
2015-04-06 15:56:54 -0700

    Display updates to admin

--- skin/models.py
+++ skin/models.py
@@ -8,11 +8,11 @@ class SiteSkin(models.Model):
     site = models.ForeignKey(Site, verbose_name=_('Site'))
     name = models.CharField(verbose_name=_('Skin Name'), max_length=20, null=False, blank=False, db_index=True)
     path = models.CharField(verbose_name=_('Path Prefix'), max_length=255, blank=True, default='',
-                            help_text=_('Enter the path to add to any existing paths template loaders will look.\n'
+                            help_text=_('Enter the path to add to any existing paths template loaders will look. '
                                         'The path used will be {base_dir}{skin_path}{template_name}.'))
 
     class Meta:
         unique_together = ('site', 'name')
 
     def __unicode__(self):
-        return '<SiteSkin: {site}.{name} - {path}>'.format(self.site.name, self.name, self.path)
+        return '<SiteSkin: {site}.{name} - {path}>'.format(site=self.site.name, name=self.name, path=self.path)
