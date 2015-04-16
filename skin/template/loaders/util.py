e943014f26d35ca76a4491f9162d658b1c79f56f
David Watkins <dwatkins@insuremonkey.com>
2015-04-15 17:11:01 -0700

    cache skins by site name and skin name

--- skin/template/loaders/util.py
+++ skin/template/loaders/util.py
@@ -1,6 +1,9 @@
 from skin.models import SiteSkin
 
 
+skin_cache = {}
+
+
 def get_site_skin(site, name):
     """ Return the skin for the requested site
     :param site: Site you are requesting skin for
@@ -10,7 +13,11 @@ def get_site_skin(site, name):
     :return: Requested skin based on site and name
     :rtype: skin.models.SiteSkin | None
     """
-    try:
-        return SiteSkin.objects.get(site=site, name=name)
-    except SiteSkin.DoesNotExist:
-        return None
+    if site.name not in skin_cache or name not in skin_cache[site.name]:
+        if site.name not in skin_cache:
+            skin_cache[site.name] = {}
+        try:
+            skin_cache[site.name][name] = SiteSkin.objects.get(site=site, name=name)
+        except SiteSkin.DoesNotExist:
+            return None
+    return skin_cache[site.name][name]
